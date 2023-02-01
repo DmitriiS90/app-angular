f1d526f4c91a10ec7aa2c959d821408162bfa13d
dschegolev <dschegolev@mmtr.ru>
2023-02-01 12:39:16 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { ProductService } from './services/product.service';
+// import { products as data} from './data/products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  // OnInit - метод жизненого цикла компонента
   title = 'app-angular';
+
+  // products: IProduct[] = data;
+  products: IProduct[] = [];
+
+  constructor(private productsService: ProductService){}
+  ngOnInit():void {
+    this.productsService.getAll().subscribe((products)=>{
+      this.products = products
+    })
+  }
 }
